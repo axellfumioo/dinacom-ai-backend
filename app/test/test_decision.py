1ad9a5a55e927dafec042cb4a28055f83fe4134c
Axel Azhar P <axelazharputra@gmail.com>
2025-12-24 19:07:47 +0700

    feat(decision): adding test case

---
+++ app/test/test_decision.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from app.ai.decision import DecisionService
+import time
+
+decision = DecisionService()
+
+test = "harga emas hari ini"
+
+start_time = time.time()
+result = decision.run(test)
+end_time = time.time()
+elapsed_time = (end_time - start_time) * 1000
+print("\nUSER:", test)
+print("DECISION:", result)
+print(f"Processing time: {elapsed_time:.2f} ms")
